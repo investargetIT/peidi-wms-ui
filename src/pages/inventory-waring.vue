c5c66db590802092d89839308043e7464e8c5488
chenbin <chenbin0417@126.com>
2024-08-05 14:09:46 +0800

    库存预警页面

---
+++ src/pages/inventory-waring.vue
@@ -0,0 +1,13 @@
+<script setup>
+import InventoryWaring from '@/views/pages/tables/InventoryWaring.vue'
+</script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <VCard title="库存预警">
+        <InventoryWaring />
+      </VCard>
+    </VCol>
+  </VRow>
+</template>
