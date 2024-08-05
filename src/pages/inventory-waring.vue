d52df83ad7cdad4e6735e3052cd645b6eabf3be2
Oliver <olimovier@gmail.com>
2024-08-05 16:34:03 +0800

    添加环境变量

--- src/pages/inventory-waring.vue
+++ src/pages/inventory-waring.vue
@@ -1,5 +1,7 @@
 <script setup>
-import InventoryWaring from '@/views/pages/tables/InventoryWaring.vue'
+import InventoryWaring from '@/views/pages/tables/InventoryWaring.vue';
+const BASE_URL = import.meta.env.VITE_API_ENDPOINT;
+console.log(BASE_URL);
 </script>
 
 <template>
