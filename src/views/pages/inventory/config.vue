3e5bbb18bf60b6f6dec191cd85160b949bf3129b
Oliver <olimovier@gmail.com>
2024-08-10 14:09:48 +0800

    完成参数配置页面

--- src/views/pages/inventory/config.vue
+++ src/views/pages/inventory/config.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="container">
-    <div class="item">
-      <label class="typo__label">名称</label>
-      <multiselect v-model="value" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name"
-        track-by="code" :options="options" :multiple="true" :taggable="true" @tag="addTag" />
+    <div v-for="item in configs" :key="item.fields" class="item">
+      <label class="typo__label">{{ item.label }}</label>
+      <multiselect tag-placeholder="添加新标签" placeholder="搜索或添加标签" deselectLabel="回车删除" :options="item.value"
+        v-model="item.value" :multiple="true" :taggable="true" :show-no-options="false" @tag="addTag($event, item)" />
     </div>
-    <v-btn>保存</v-btn>
+    <v-btn @click="save">保存</v-btn>
+    <v-dialog v-model="dialog" width="auto">
+      <v-card min-width="400" title="保存成功">
+        <template v-slot:actions>
+          <v-btn class="ms-auto" text="OK" @click="dialog = false"></v-btn>
+        </template>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Multiselect from 'vue-multiselect';
+const BASE_URL = import.meta.env.VITE_API_ENDPOINT;
 
 export default {
   components: {
@@ -18,24 +27,37 @@ export default {
   },
   data() {
     return {
-      value: [
-        { name: 'Javascript', code: 'js' }
-      ],
-      options: [
-        { name: 'Vue.js', code: 'vu' },
-        { name: 'Javascript', code: 'js' },
-        { name: 'Open Source', code: 'os' }
-      ],
+      configs: [],
+      dialog: false,
+      text: '',
     };
   },
+  mounted() {
+    axios.get(BASE_URL + '/inventory/config')
+      .then(response => {
+        const { code, data } = response.data;
+        if (code == 200) {
+          this.configs = data.slice();
+        }
+      })
+      .catch(error => {
+        console.error("API调用错误：", error);
+      });
+  },
   methods: {
-    addTag(newTag) {
-      const tag = {
-        name: newTag,
-        code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
-      }
-      this.options.push(tag)
-      this.value.push(tag)
+    addTag(newTag, item) {
+      item.value.push(newTag);
+    },
+    save() {
+      axios.post(BASE_URL + '/inventory/config', this.configs)
+        .then(response => {
+          console.log(response.data);
+          this.text = '保存成功';
+          this.dialog = true;
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
   }
 };
