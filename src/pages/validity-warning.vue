6ff245f279acd09d429c71079070eb11034d2a6b
Oliver <olimovier@gmail.com>
2024-08-12 19:46:33 +0800

    Update text

--- src/pages/validity-warning.vue
+++ src/pages/validity-warning.vue
@@ -5,7 +5,7 @@ import ValidityWarning from '@/views/pages/tables/ValidityWarning.vue';
 <template>
   <VRow>
     <VCol cols="12">
-      <VCard title="有效期预警">
+      <VCard title="效期预警">
         <ValidityWarning />
       </VCard>
     </VCol>
