2b5a6fdda89594aaa2b81609fca39f8c7d7971c4
Liu Hanjia <13099740@qq.com>
2024-08-14 15:13:25 +0800

    修改登陆界面样式

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,11 +1,5 @@
 <script setup>
-import { useTheme } from 'vuetify'
-import AuthProvider from '@/views/pages/authentication/AuthProvider.vue'
-import logo from '@images/logo.svg?raw'
-import authV1MaskDark from '@images/pages/auth-v1-mask-dark.png'
-import authV1MaskLight from '@images/pages/auth-v1-mask-light.png'
-import authV1Tree2 from '@images/pages/auth-v1-tree-2.png'
-import authV1Tree from '@images/pages/auth-v1-tree.png'
+import { useTheme } from 'vuetify';
 
 const form = ref({
   email: '',
@@ -15,9 +9,6 @@ const form = ref({
 
 const vuetifyTheme = useTheme()
 
-const authThemeMask = computed(() => {
-  return vuetifyTheme.global.name.value === 'light' ? authV1MaskLight : authV1MaskDark
-})
 
 const isPasswordVisible = ref(false)
 </script>
@@ -25,136 +16,84 @@ const isPasswordVisible = ref(false)
 <template>
   <!-- eslint-disable vue/no-v-html -->
 
-  <div class="auth-wrapper d-flex align-center justify-center pa-4">
-    <VCard
-      class="auth-card pa-4 pt-7"
-      max-width="448"
-    >
-      <VCardItem class="justify-center">
-        <RouterLink
-          to="/"
-          class="d-flex align-center gap-3"
-        >
+  <div class="auth-wrapper d-flex align-center justify-center pa-4" style="min-height: 100vh; /* 确保容器至少占据整个视口的高度 */
+  background: url('src/assets/images/pages/login.jpeg') no-repeat center bottom / cover;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;">
+    <VCard class="auth-card pa-4 pt-7"
+      style="width: 300px; max-width: 90vw; max-height: 600px;margin-left: auto; margin-right:2.3%;">
+      <VCardItem class="d-flex flex-column align-start">
+        <RouterLink to="/" class="d-flex align-center gap-3">
           <!-- eslint-disable vue/no-v-html -->
-          <div
-            class="d-flex"
-            v-html="logo"
-          />
-          <h2 class="font-weight-medium text-2xl text-uppercase">
-            Materio
+          <!-- <div class="d-flex" v-html="logo" /> -->
+          <h2 class="font-weight-medium text-2xl text-uppercase" style="text-align: left;">
+            欢迎登陆
           </h2>
         </RouterLink>
       </VCardItem>
 
       <VCardText class="pt-2">
-        <h4 class="text-h4 mb-1">
+        <!-- <h4 class="text-h4 mb-1">
           Welcome to Materio! 👋🏻
-        </h4>
-        <p class="mb-0">
+        </h4> -->
+        <!-- <p class="mb-0">
           Please sign-in to your account and start the adventure
-        </p>
+        </p> -->
       </VCardText>
 
       <VCardText>
-        <VForm @submit.prevent="() => {}">
+        <VForm @submit.prevent="() => { }">
           <VRow>
             <!-- email -->
             <VCol cols="12">
-              <VTextField
-                v-model="form.email"
-                label="Email"
-                type="email"
-              />
+              <VTextField v-model="form.email" label="用户名" type="email" class="mb-4" />
             </VCol>
 
             <!-- password -->
             <VCol cols="12">
-              <VTextField
-                v-model="form.password"
-                label="Password"
-                placeholder="············"
+              <VTextField v-model="form.password" label="密码" placeholder="············"
                 :type="isPasswordVisible ? 'text' : 'password'"
                 :append-inner-icon="isPasswordVisible ? 'ri-eye-off-line' : 'ri-eye-line'"
-                @click:append-inner="isPasswordVisible = !isPasswordVisible"
-              />
-
+                @click:append-inner="isPasswordVisible = !isPasswordVisible" class="mb-4" />
               <!-- remember me checkbox -->
-              <div class="d-flex align-center justify-space-between flex-wrap my-6">
-                <VCheckbox
-                  v-model="form.remember"
-                  label="Remember me"
-                />
+              <!-- <div class="d-flex align-center justify-space-between flex-wrap my-6">
+                <VCheckbox v-model="form.remember" label="Remember me" />
 
-                <a
-                  class="text-primary"
-                  href="javascript:void(0)"
-                >
+                <a class="text-primary" href="javascript:void(0)">
                   Forgot Password?
                 </a>
-              </div>
+              </div> -->
 
               <!-- login button -->
-              <VBtn
-                block
-                type="submit"
-                to="/"
-              >
-                Login
+              <VBtn block type="submit">
+                登陆
               </VBtn>
             </VCol>
 
             <!-- create account -->
-            <VCol
-              cols="12"
-              class="text-center text-base"
-            >
+            <!-- <VCol cols="12" class="text-center text-base">
               <span>New on our platform?</span>
-              <RouterLink
-                class="text-primary ms-2"
-                to="/register"
-              >
+              <RouterLink class="text-primary ms-2" to="/register">
                 Create an account
               </RouterLink>
             </VCol>
 
-            <VCol
-              cols="12"
-              class="d-flex align-center"
-            >
+            <VCol cols="12" class="d-flex align-center">
               <VDivider />
               <span class="mx-4">or</span>
               <VDivider />
-            </VCol>
+            </VCol> -->
 
             <!-- auth providers -->
-            <VCol
-              cols="12"
-              class="text-center"
-            >
+            <!-- <VCol cols="12" class="text-center">
               <AuthProvider />
-            </VCol>
+            </VCol> -->
           </VRow>
         </VForm>
       </VCardText>
     </VCard>
 
-    <VImg
-      class="auth-footer-start-tree d-none d-md-block"
-      :src="authV1Tree"
-      :width="250"
-    />
-
-    <VImg
-      :src="authV1Tree2"
-      class="auth-footer-end-tree d-none d-md-block"
-      :width="350"
-    />
-
-    <!-- bg img -->
-    <VImg
-      class="auth-footer-mask d-none d-md-block"
-      :src="authThemeMask"
-    />
   </div>
 </template>
 
