db26ad5dcb1bcea0e047fd65d7f22967266cc426
chenbin <chenbin0417@126.com>
2024-08-20 16:35:34 +0800

    库存预警和效期预警增加搜索功能

--- src/views/pages/tables/InventoryWarning.vue
+++ src/views/pages/tables/InventoryWarning.vue
@@ -5,6 +5,16 @@ const BASE_URL = import.meta.env.VITE_API_ENDPOINT;
 export default {
   data() {
     return {
+      searchQuery: {
+        brandName: '',
+        specNo: '',
+        goodsName: '',
+        specName: '',
+        inventoryNum: '',
+        turnoverDays: '',
+        groupType: '',
+        waringLevel: '',
+      },
       headers: [
         { title: '品牌', value: 'brandName', sortable: true },
         { title: '商品编码', value: 'specNo', sortable: false },
@@ -20,7 +30,38 @@ export default {
       ],
     };
   },
+  computed: {
+    searchParams() {
+      return [
+        { searchName: 'brandName', searchType: 'like', searchValue: this.searchQuery.brandName },
+        { searchName: 'specNo', searchType: 'like', searchValue: this.searchQuery.specNo },
+        { searchName: 'goodsName', searchType: 'like', searchValue: this.searchQuery.goodsName },
+        { searchName: 'specName', searchType: 'like', searchValue: this.searchQuery.specName },
+        { searchName: 'inventoryNum', searchType: 'like', searchValue: this.searchQuery.inventoryNum },
+        { searchName: 'turnoverDays', searchType: 'like', searchValue: this.searchQuery.turnoverDays },
+        { searchName: 'groupType', searchType: 'like', searchValue: this.searchQuery.groupType },
+        { searchName: 'waringLevel', searchType: 'like', searchValue: this.searchQuery.waringLevel },
+      ].filter(param => param.searchValue.trim() !== '')
+    }
+  },
   methods: {
+    onSearch() {
+      // 在这里实现你的搜索逻辑
+      // console.log('Searching with:', this.searchQuery);
+      const encodedParams = encodeURIComponent(JSON.stringify(this.searchParams));
+      const url = `http://wmsapi.peidigroup.cn/inventory/inventory-waring?searchStr=${encodedParams}`;
+      fetch(url)
+        .then(response => response.json())
+        .then(responseData => {
+          const { code, data } = responseData;
+          if (code == 200) {
+            this.items = data;
+          }
+        })
+        .catch(error => {
+          console.error('API调用错误:', error);
+        });
+    },
     updateData() {
       axios.get(BASE_URL + '/inventory/inventory-waring')
         .then(response => {
@@ -29,7 +70,7 @@ export default {
             this.items = data;
           }
           // 成功时的处理逻辑
-          console.log(this.items);
+          // console.log(this.items);
         })
         .catch(error => {
           // 错误处理
@@ -49,6 +90,39 @@ export default {
 </script>
 
 <template>
+  <v-form @submit.prevent="onSearch">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.brandName" label="品牌" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.specNo" label="商品编码" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.goodsName" label="货品名称" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.specName" label="规格名称" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.inventoryNum" label="库存数量" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.turnoverDays" label="预计可周转天数" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.groupType" label="分类" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.waringLevel" label="库存预警" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-btn type="submit" color="primary">搜索</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
   <v-data-table :headers="headers" :items="items" :items-per-page="-1" :hide-default-footer="true">
     <template v-slot:item.waringLevel="{ item }">
       <span class="circle" :style="{ 'background': item.waringLevel }"></span>
--- src/views/pages/tables/ValidityWarning.vue
+++ src/views/pages/tables/ValidityWarning.vue
@@ -5,6 +5,17 @@ const BASE_URL = import.meta.env.VITE_API_ENDPOINT;
 export default {
   data() {
     return {
+      searchQuery: {
+        brandName: '',
+        specNo: '',
+        goodsName: '',
+        specName: '',
+        groupType: '',
+        waring1Num: '',
+        waring2Num: '',
+        waring3Num: '',
+        inventoryNum: '',
+      },
       headers: [
         { title: '品牌', value: 'brandName', sortable: true, },
         { title: '商品编码', value: 'specNo', sortable: false },
@@ -21,7 +32,39 @@ export default {
       ],
     };
   },
+  computed: {
+    searchParams() {
+      return [
+        { searchName: 'brandName', searchType: 'like', searchValue: this.searchQuery.brandName },
+        { searchName: 'specNo', searchType: 'like', searchValue: this.searchQuery.specNo },
+        { searchName: 'goodsName', searchType: 'like', searchValue: this.searchQuery.goodsName },
+        { searchName: 'specName', searchType: 'like', searchValue: this.searchQuery.specName },
+        { searchName: 'groupType', searchType: 'like', searchValue: this.searchQuery.groupType },
+        { searchName: 'waring1Num', searchType: 'like', searchValue: this.searchQuery.waring1Num },
+        { searchName: 'waring2Num', searchType: 'like', searchValue: this.searchQuery.waring2Num },
+        { searchName: 'waring3Num', searchType: 'like', searchValue: this.searchQuery.waring3Num },
+        { searchName: 'inventoryNum', searchType: 'like', searchValue: this.searchQuery.inventoryNum },
+      ].filter(param => param.searchValue.trim() !== '')
+    }
+  },
   methods: {
+    onSearch() {
+      // 在这里实现你的搜索逻辑
+      // console.log('Searching with:', this.searchQuery);
+      const encodedParams = encodeURIComponent(JSON.stringify(this.searchParams));
+      const url = `http://wmsapi.peidigroup.cn/inventory/validity-waring?searchStr=${encodedParams}`;
+      fetch(url)
+        .then(response => response.json())
+        .then(responseData => {
+          const { code, data } = responseData;
+          if (code == 200) {
+            this.items = data;
+          }
+        })
+        .catch(error => {
+          console.error('API调用错误:', error);
+        });
+    },
     updateData() {
       axios.get(BASE_URL + '/inventory/validity-waring')
         .then(response => {
@@ -30,7 +73,7 @@ export default {
             this.items = data;
           }
           // 成功时的处理逻辑
-          console.log(this.items);
+          // console.log(this.items);
         })
         .catch(error => {
           // 错误处理
@@ -50,6 +93,42 @@ export default {
 </script>
 
 <template>
+  <v-form @submit.prevent="onSearch">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.brandName" label="品牌" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.specNo" label="商品编码" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.goodsName" label="货品名称" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.specName" label="规格名称" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.groupType" label="分类" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.waring1Num" label="1/3效期" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.waring2Num" label="2/3效期" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.waring3Num" label="临期" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-text-field v-model="searchQuery.inventoryNum" label="库存数量" type="text" outlined dense></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4" sm="6">
+          <v-btn type="submit" color="primary">搜索</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
   <v-data-table :headers="headers" :items="items" :items-per-page="-1" :hide-default-footer="true">
   </v-data-table>
 </template>
