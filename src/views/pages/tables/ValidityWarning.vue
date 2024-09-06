00c235fb4ce9ca8f4289654fcbb3790a4f7c29d2
Oliver <olimovier@gmail.com>
2024-09-06 09:27:59 +0800

    效期预警全筛

--- src/views/pages/tables/ValidityWarning.vue
+++ src/views/pages/tables/ValidityWarning.vue
@@ -5,16 +5,23 @@ const BASE_URL = import.meta.env.VITE_API_ENDPOINT;
 export default {
   data() {
     return {
+      defaultSort: [
+        { sortName: 'waring3Num', sortType: 'desc' },
+        { sortName: 'waring2Num', sortType: 'desc' },
+        { sortName: 'waring1Num', sortType: 'desc' },
+        { sortName: 'groupType', sortType: 'desc' },
+        { sortName: 'brandName', sortType: 'desc' },
+      ],
       searchQuery: {
         brandName: null,
-        specNo: '',
-        goodsName: '',
-        specName: '',
-        groupType: '',
-        waring1Num: '',
-        waring2Num: '',
-        waring3Num: '',
-        inventoryNum: '',
+        specNo: null,
+        goodsName: null,
+        specName: null,
+        groupType: null,
+        waring1Num: [0, 1000000],
+        waring2Num: [0, 100000],
+        waring3Num: [0, 100000],
+        inventoryNum: [0, 1000000],
       },
       headers: [
         { title: '品牌', value: 'brandName', sortable: true, },
@@ -27,9 +34,10 @@ export default {
         { title: '临期', value: 'waring3Num', sortable: true, minWidth: 100 },
         { title: '库存数量', value: 'inventoryNum', sortable: true, minWidth: 100 },
       ],
-      items: [
-
-      ],
+      items: [],
+      allSpecNo: [],
+      allGoodsName: [],
+      allSpecName: [],
     };
   },
   computed: {
@@ -40,10 +48,10 @@ export default {
         { searchName: 'goodsName', searchType: 'like', searchValue: this.searchQuery.goodsName },
         { searchName: 'specName', searchType: 'like', searchValue: this.searchQuery.specName },
         { searchName: 'groupType', searchType: 'like', searchValue: this.searchQuery.groupType },
-        { searchName: 'waring1Num', searchType: 'like', searchValue: this.searchQuery.waring1Num },
-        { searchName: 'waring2Num', searchType: 'like', searchValue: this.searchQuery.waring2Num },
-        { searchName: 'waring3Num', searchType: 'like', searchValue: this.searchQuery.waring3Num },
-        { searchName: 'inventoryNum', searchType: 'like', searchValue: this.searchQuery.inventoryNum },
+        { searchName: 'waring1Num', searchType: 'between', searchValue: this.searchQuery.waring1Num.join('#/#') },
+        { searchName: 'waring2Num', searchType: 'between', searchValue: this.searchQuery.waring2Num.join('#/#') },
+        { searchName: 'waring3Num', searchType: 'between', searchValue: this.searchQuery.waring3Num.join('#/#') },
+        { searchName: 'inventoryNum', searchType: 'between', searchValue: this.searchQuery.inventoryNum.join('#/#') },
       ].filter(param => param.searchValue && param.searchValue.trim() !== '')
     }
   },
@@ -52,7 +60,8 @@ export default {
       // 在这里实现你的搜索逻辑
       // console.log('Searching with:', this.searchQuery);
       const encodedParams = encodeURIComponent(JSON.stringify(this.searchParams));
-      const url = `http://wmsapi.peidigroup.cn/inventory/validity-waring?searchStr=${encodedParams}`;
+      const encodedSort = encodeURIComponent(JSON.stringify(this.defaultSort));
+      const url = `http://wmsapi.peidigroup.cn/inventory/validity-waring?searchStr=${encodedParams}&sortStr=${encodedSort}`;
       fetch(url)
         .then(response => response.json())
         .then(responseData => {
@@ -66,18 +75,14 @@ export default {
         });
     },
     updateData() {
-      const defaultSort = [
-        { sortName: 'waring3Num', sortType: 'desc' },
-        { sortName: 'waring2Num', sortType: 'desc' },
-        { sortName: 'waring1Num', sortType: 'desc' },
-        { sortName: 'groupType', sortType: 'desc' },
-        { sortName: 'brandName', sortType: 'desc' },
-      ];
-      axios.get(BASE_URL + '/inventory/validity-waring?sortStr=' + encodeURIComponent(JSON.stringify(defaultSort)))
+      axios.get(BASE_URL + '/inventory/validity-waring?sortStr=' + encodeURIComponent(JSON.stringify(this.defaultSort)))
         .then(response => {
           const { code, data } = response.data;
           if (code == 200) {
             this.items = data;
+            this.allSpecNo = data.map(m => m.specNo);
+            this.allGoodsName = data.map(m => m.goodsName);
+            this.allSpecName = data.map(m => m.specName);
           }
           // 成功时的处理逻辑
           // console.log(this.items);
@@ -87,14 +92,23 @@ export default {
           console.error("API调用错误：", error);
         });
     },
+    handleRestBtnClicked() {
+      this.searchQuery = {
+        brandName: null,
+        specNo: null,
+        goodsName: null,
+        specName: null,
+        groupType: null,
+        waring1Num: [0, 1000000],
+        waring2Num: [0, 100000],
+        waring3Num: [0, 100000],
+        inventoryNum: [0, 1000000],
+      };
+      this.updateData();
+    },
   },
   mounted() {
-    // 当组件挂载后，更新数据
     this.updateData();
-    // 设置定时器模拟数据更新 (实际应用中应该避免使用setInterval)
-    // setInterval(() => {
-    //   this.updateData();
-    // }, 5000); // 每5秒更新一次数据
   },
 };
 </script>
@@ -103,36 +117,76 @@ export default {
   <v-form @submit.prevent="onSearch">
     <v-container>
       <v-row>
-        <v-col cols="12" md="4" sm="6">
+        <v-col cols="12" md="6">
           <v-select v-model="searchQuery.brandName" clearable label="品牌"
             :items="['SMARTBONES', '好适嘉', '齿能', 'Meatyway爵宴']" />
         </v-col>
-        <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.specNo" label="商品编码" type="text" outlined dense></v-text-field>
+        <v-col cols="12" md="6">
+          <v-select v-model="searchQuery.groupType" clearable label="分类" :items="['A', 'B', 'C']" />
         </v-col>
         <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.goodsName" label="货品名称" type="text" outlined dense></v-text-field>
+          <v-autocomplete v-model="searchQuery.specNo" clearable label="商品编码" :items="allSpecNo" />
         </v-col>
         <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.specName" label="规格名称" type="text" outlined dense></v-text-field>
+          <v-autocomplete v-model="searchQuery.goodsName" clearable label="货品名称" :items="allGoodsName" />
         </v-col>
         <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.groupType" label="分类" type="text" outlined dense></v-text-field>
+          <v-autocomplete v-model="searchQuery.specName" clearable label="规格名称" :items="allSpecName" />
         </v-col>
-        <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.waring1Num" label="1/3效期" type="text" outlined dense></v-text-field>
+        <v-col cols="12">
+          <v-range-slider v-model="searchQuery.waring1Num" :max="1000000" :min="0" :step="1" label="1/3效期"
+            class="align-center" hide-details>
+            <template v-slot:prepend>
+              <v-text-field v-model="searchQuery.waring1Num[0]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+            <template v-slot:append>
+              <v-text-field v-model="searchQuery.waring1Num[1]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+          </v-range-slider>
         </v-col>
-        <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.waring2Num" label="2/3效期" type="text" outlined dense></v-text-field>
+        <v-col cols="12">
+          <v-range-slider v-model="searchQuery.waring2Num" :max="100000" :min="0" :step="1" label="2/3效期"
+            class="align-center" hide-details>
+            <template v-slot:prepend>
+              <v-text-field v-model="searchQuery.waring2Num[0]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+            <template v-slot:append>
+              <v-text-field v-model="searchQuery.waring2Num[1]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+          </v-range-slider>
         </v-col>
-        <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.waring3Num" label="临期" type="text" outlined dense></v-text-field>
+        <v-col cols="12">
+          <v-range-slider v-model="searchQuery.waring3Num" :max="100000" :min="0" :step="1" label="临期"
+            class="align-center" hide-details>
+            <template v-slot:prepend>
+              <v-text-field v-model="searchQuery.waring3Num[0]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+            <template v-slot:append>
+              <v-text-field v-model="searchQuery.waring3Num[1]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+          </v-range-slider>
         </v-col>
+        <v-col cols="12">
+          <v-range-slider v-model="searchQuery.inventoryNum" :max="1000000" :min="0" :step="1" label="库存数量"
+            class="align-center" hide-details>
+            <template v-slot:prepend>
+              <v-text-field v-model="searchQuery.inventoryNum[0]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+            <template v-slot:append>
+              <v-text-field v-model="searchQuery.inventoryNum[1]" density="compact" style="width: 120px" type="number"
+                variant="outlined" hide-details single-line></v-text-field>
+            </template>
+          </v-range-slider> </v-col>
         <v-col cols="12" md="4" sm="6">
-          <v-text-field v-model="searchQuery.inventoryNum" label="库存数量" type="text" outlined dense></v-text-field>
-        </v-col>
-        <v-col cols="12" md="4" sm="6">
-          <v-btn type="submit" color="primary">搜索</v-btn>
+          <v-btn type="submit" color="primary" style="margin-right: 10px;">搜索</v-btn>
+          <v-btn color="secondary" @click="handleRestBtnClicked">重置</v-btn>
         </v-col>
       </v-row>
     </v-container>
